3319375c1aa8fd8dbabb0ac83bf9b02343951e17
Hans-Christian Otto <c.otto@suora.com>
2023-05-15 15:11:12 +0200

    Add more tests for proejcts

--- src/page/Projects.test.tsx
+++ src/page/Projects.test.tsx
@@ -1,6 +1,21 @@
 import Projects from "./Projects";
 import { render } from "../test-utils";
+import { screen } from "@testing-library/react";
 
 it("renders without crashing", () => {
   render(<Projects />);
+});
+
+it("initially shows a loading indicator", () => {
+  render(<Projects />);
+
+  expect(screen.getByText(/loading/i)).toBeInTheDocument();
+});
+
+it("renders a project after loading the data", async () => {
+  render(<Projects />);
+
+  const projectElement = await screen.findByText("iJS Conference");
+
+  expect(projectElement).toBeVisible();
 });
